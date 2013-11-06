2bac03dc7204bd83e7c190f04565677b932769b3
tomoyuki-yamaguchi <tomoyuki.yamaguchi@gree.net>
2013-11-06 16:30:30 +0900

    [zsh] PATH にmysqlを追加、rbenvの初期化コードを追加

--- .oh-my-zsh/custom/custom-global.zsh
+++ .oh-my-zsh/custom/custom-global.zsh
@@ -1,8 +1,11 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
+MYSQL=/opt/mysql/server-5.6/bin
 
 export EDITOR="emacsclient"
 export LANG="ja_JP.UTF-8"
 export LC_CTYPE="ja_JP.UTF-8"
 export LESSCHARSET=utf-8
+export PATH=$MYSQL:$HOME/bin:$HOME/.rbenv/bin:/sbin:$PATH
+eval "$(rbenv init -)"
