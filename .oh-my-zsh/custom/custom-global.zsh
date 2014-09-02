b0c56f04b8f36201077745b8c3f31c33d9497db9
tomoyuki-yamaguchi <tomoyuki.yamaguchi@gree.net>
2014-09-02 12:37:51 +0900

    node用のpath追加

--- .oh-my-zsh/custom/custom-global.zsh
+++ .oh-my-zsh/custom/custom-global.zsh
@@ -2,6 +2,7 @@ HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
 MYSQL=/opt/mysql/server-5.6/bin
+NODE=$HOME/.nodebrew/current/bin
 
 unsetopt correct
 unsetopt correct_all
@@ -14,5 +15,5 @@ export EDITOR="emacsclient"
 export LANG="ja_JP.UTF-8"
 export LC_CTYPE="ja_JP.UTF-8"
 export LESSCHARSET=utf-8
-export PATH=$MYSQL:$HOME/bin:/sbin:/usr/local/bin/:./vendor/binstubs:$HOME/.rbenv/bin:${HOME}/.rbenv/shims:$PATH
+export PATH=$MYSQL:$HOME/bin:/sbin:/usr/local/bin:./vendor/binstubs:$HOME/.rbenv/bin:${HOME}/.rbenv/shims:$NODE:$PATH
 eval "$(rbenv init -)"
