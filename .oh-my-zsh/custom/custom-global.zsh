0f2d50f704e461d863668012a1440e45c960c230
tomoyuki-yamaguchi <tomoyuki.yamaguchi@gree.net>
2014-12-26 19:00:12 +0900

    [zsh] update PATH

--- .oh-my-zsh/custom/custom-global.zsh
+++ .oh-my-zsh/custom/custom-global.zsh
@@ -15,5 +15,5 @@ export EDITOR="emacsclient"
 export LANG="ja_JP.UTF-8"
 export LC_CTYPE="ja_JP.UTF-8"
 export LESSCHARSET=utf-8
-export PATH=$MYSQL:$HOME/bin:/sbin:/usr/local/bin:./vendor/binstubs:$HOME/.rbenv/bin:${HOME}/.rbenv/shims:$NODE:$PATH
+export PATH=$HOME/.rbenv/bin:${HOME}/.rbenv/shims:$MYSQL:$HOME/bin:/sbin:/usr/local/bin:/usr/bin:./vendor/binstubs:$NODE:$PATH
 eval "$(rbenv init -)"
